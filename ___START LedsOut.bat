d758aef2c83fd070d929caf8d5c460c02ef795d0
Lasal <aristides_job@hotmail.com>
2018-03-17 21:43:44 +0100

    Delete ___START LedsOut.bat

--- ___START LedsOut.bat
+++
@@ -1,5 +0,0 @@
-@echo OFF
-@echo --------------MidiLedsOut----------------
-timeout 1
-@echo OFF
-start "" "%~dp0vvvv_50alpha35.18_x64\vvvv.exe" /allowmultiple /showexceptions 0 /o ..\MidiLedsOut.v4p
