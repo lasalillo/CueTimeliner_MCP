43b9bcf720a41d1a1ddf64e5267f27b7bd3bbed2
Aristides Garcia <aristides_job@hotmail.com>
2018-04-16 19:34:03 +0200

    Midi OUT update, Clean up Please update the .bat files, no MIDI IN / OUT instances needed anymore

--- ___MIDI_OUT_Icon.bat
+++
@@ -1,8 +0,0 @@
-@echo --------------MIDI OUT----------------
-@echo OFF
- start "" "%~dp0vvvv_50beta36_x64\vvvv.exe" /showexceptions 0 /allowmultiple  /o ..\CueTimeliner_MCP\MIDI_OUT_Icon.v4p
-
-
-  
-
-
