833fb611a6d860cbb00114b2fe06aaf04e30832b
Aristides Garcia <aristides_job@hotmail.com>
2018-05-03 17:47:52 +0200

    MergeProject bugfix .bat files

--- ___NETWORK.bat
+++ ___NETWORK.bat
@@ -1,6 +1,6 @@
 @echo --------------NETWORK----------------
 @echo OFF
- start "" "%~dp0vvvv_50beta36_x64\vvvv.exe"  /showexceptions 0 /allowmultiple  /o ..\CueTimeliner_MCP\SendReceiveModules\Network.v4p
+ start "" "Schumann\schumann_vvvv\vvvv_50beta36_x64\vvvv.exe"  /showexceptions 0 /allowmultiple  /o ..\..\..\CueTimeliner_MCP\SendReceiveModules\Network.v4p
 
 
 
--- ___START Audio.bat
+++ ___START Audio.bat
@@ -3,6 +3,7 @@
 timeout 1
 
 @echo --------------AudioPlayer----------------
+timeout 1
 @echo OFF
- start "" "%~dp0vvvv_50beta36_x64\vvvv.exe" /allowmultiple /showexceptions 0  /o ..\CueTimeliner_MCP\Audio\_AUDIO_PLAYER.v4p
+ start "" "Schumann\schumann_vvvv\vvvv_50beta36_x64\vvvv.exe" /allowmultiple /showexceptions 0  /o ..\..\..\CueTimeliner_MCP\Audio\_AUDIO_PLAYER.v4p
 
